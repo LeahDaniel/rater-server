8236766c7996632c67109aa3e19c60afd5815f15
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-01 10:11:17 -0600

    migrated models, chap 4 complete

---
+++ raterapi/models/__init__.py
@@ -0,0 +1,6 @@
+from .category import Category
+from .game import Game
+from .gameCategory import GameCategory
+from .gameRating import GameRating
+from .gameReview import GameReview
+from .picture import Picture
